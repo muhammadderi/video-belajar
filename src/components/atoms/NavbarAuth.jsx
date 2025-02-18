17d7171dcde46ccdcd1c74118882a280920718d1
Muhammad Dery Setiawan <45331144+muhammadderi@users.noreply.github.com>
2025-02-18 08:53:08 +0000

    fix: img path

--- src/components/atoms/NavbarAuth.jsx
+++ src/components/atoms/NavbarAuth.jsx
@@ -2,7 +2,7 @@ function NavbarAuth() {
   return (
     <nav className="md:w-screen w-[360px] h-20 border-1 flex justify-between md:py-3 md:px-32 py-[16px] px-[24px]">
       <img
-        src="\src\assets\auth\Logo.png"
+        src="/Logo/Logo.png"
         className="md:w-[237px] md:h-14 w-[142px] h-[42px]"
         alt="logo"
       />
